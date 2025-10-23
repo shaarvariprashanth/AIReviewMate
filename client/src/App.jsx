b881066625962f22d82b1e744100006108653eac
shaarvariprashanth <shaarvariprashanth@gmail.com>
2025-10-23 12:37:04 +0530

    Navbar file removed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,10 @@
 import MonacoEditor from "./components/MonacoEditor";
-import Navbar from "./components/Navbar";
-import { useState,useEffect } from "react";
 
 function App() {
-  const [theme, setTheme] = useState("light");
-
-  useEffect(() => {
-    const saved = localStorage.getItem("theme") || "light";
-    setTheme(saved);
-    document.documentElement.setAttribute("data-theme", saved);
-  }, []);
-
-  const toggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
-    document.documentElement.setAttribute("data-theme", newTheme);
-  };
 
   return (
     <div>
-     <Navbar theme={theme} toggleTheme={toggleTheme} /> 
-      <MonacoEditor theme={theme} />
+      <MonacoEditor/>
     </div>
   );
 }
